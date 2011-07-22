3da1d71ec8a9d1c5ee71a8c9e197392a67eb1a25
Alex Fort <imphasing@gmail.com>
2011-07-22 15:32:05 -0400

    Implement a basic s-expression parser. Can now parse and put into an AST basic scheme programs, next up, evaulation!

--- src/Schemin/Program.cs
+++ src/Schemin/Program.cs
@@ -5,6 +5,8 @@ namespace Schemin
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Text;
+	using Schemin.Tokenize;
+	using Schemin.Parse;
 
 	class Program
 	{
@@ -15,19 +17,61 @@ namespace Schemin
 
 			string test = @"
 (define test (lambda () (
-begin 
-(print ""test"")
-(print 1)
-)))
-(test)";
-
+   begin 
+   (print ""test"")
+   (print 1)
+ )))
+ 
+ (test)";
 			string noLiterals = t.ExtractLiterals(test, env);
 
 			List<Token> tokens = t.Tokenize(noLiterals, env);
 
-			foreach (Token token in tokens)
+			Parser p = new Parser();
+			KeyValuePair<SchemeList, int> parsed = p.Parse(tokens, 0);
+			
+			DisplayList(parsed.Key, 0);
+		}
+
+
+		static void DisplayList(SchemeList list, int level)
+		{
+			foreach (var type in list.List)
 			{
-				Console.WriteLine(string.Format("Token: {0}, Type: {1}", token.Value, token.Type));
+				if (type.GetType() == typeof(SchemeList))
+				{
+					Console.WriteLine("Descending into a list...");
+					DisplayList((SchemeList) type, level + 1);
+				}
+				else
+				{
+					string value = String.Empty;
+					string levelPadding = String.Empty;
+
+					for (int i = 0; i < level; i++)
+					{
+						levelPadding += "        ";
+					}
+
+					if (type.GetType() == typeof(SchemeAtom))
+					{
+						SchemeAtom atom = (SchemeAtom) type;	
+						value = atom.Name;
+					}
+					else if (type.GetType() == typeof(SchemeString))
+					{
+						SchemeString str = (SchemeString) type;
+						value = str.Value;
+					}
+					else if (type.GetType() == typeof(SchemeInteger))
+					{
+						SchemeInteger num = (SchemeInteger) type;
+						value = num.Value.ToString();
+					}
+
+
+					Console.WriteLine(string.Format("{0}Type: {1} Value: {2}", levelPadding, type.GetType().ToString(), value));
+				}
 			}
 		}
 	}
