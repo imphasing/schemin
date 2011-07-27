fde5db568f2134d114340119b0b76aab7e0740e3
Alex Fort <imphasing@gmail.com>
2011-07-26 20:27:50 -0400

    Started implementing lambdas.. this doesn't work yet.

--- src/Schemin/Schemin.AST/ScheminLambda.cs
+++ src/Schemin/Schemin.AST/ScheminLambda.cs
@@ -2,6 +2,8 @@
 namespace Schemin.AST
 {
 	using System;
+	using Schemin.Evaluate;
+	using Environment = Schemin.Evaluate.Environment;
 
 	public class ScheminLambda : IScheminType
 	{
@@ -12,8 +14,31 @@ namespace Schemin.AST
 			this.Definition = definition;
 		}
 
-		public IScheminType Evaluate(ScheminList args)
+		public IScheminType Evaluate(ScheminList args, Evaluator eval, Environment env)
 		{
+			ScheminList symbols = (ScheminList) args.Car();
+			ScheminList definition = (ScheminList) args.Cdr();
+
+			ScheminAtom first;
+			ScheminList rest;
+
+			ScheminAtom firstArg;
+			ScheminList restArgs;
+
+			for (; ;)
+			{
+				first = (ScheminAtom) symbols.Car();
+				firstArg = (ScheminAtom) args.Car();
+				rest = symbols.Cdr();
+				restArgs = args.Cdr();
+
+					
+
+				first = rest.Car();
+				firstArg = restArgs.Car();
+				rest = rest.Cdr();
+				restArgs = restArgs.Cdr();
+			}
 			throw new NotImplementedException();	
 		}
 	}
