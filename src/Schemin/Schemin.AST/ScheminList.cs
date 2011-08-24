aa2330c0c04e7dea3efd7ed4beb1acf3e7e57925
Alex Fort <imphasing@gmail.com>
2011-08-24 18:58:41 -0400

    Added BSD license headers, fixed indenting on this crazy mix of Vim and VS indenting.

--- src/Schemin/Schemin.AST/ScheminList.cs
+++ src/Schemin/Schemin.AST/ScheminList.cs
@@ -1,3 +1,29 @@
+/* 
+ * Copyright (c) 2011 Alex Fort 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 namespace Schemin.AST
 {
@@ -12,9 +38,9 @@ namespace Schemin.AST
 		public ScheminList Rest = null;
 		public bool Empty = true;
 
-        private bool quoted = true;
+		private bool quoted = true;
 
-        public static bool QuoteLists = true;
+		public static bool QuoteLists = true;
 
 		public int Length
 		{
@@ -44,20 +70,20 @@ namespace Schemin.AST
 
 		public ScheminList()
 		{
-            if (!QuoteLists)
-            {
-                this.quoted = false;
-            }
+			if (!QuoteLists)
+			{
+				this.quoted = false;
+			}
 
 			this.Empty = true;
 		}
 
 		public ScheminList(IScheminType head)
 		{
-            if (!QuoteLists)
-            {
-                this.quoted = false;
-            }
+			if (!QuoteLists)
+			{
+				this.quoted = false;
+			}
 
 			this.Head = head;
 			this.Empty = false;
@@ -65,10 +91,10 @@ namespace Schemin.AST
 
 		public ScheminList(IScheminType head, ScheminList rest)
 		{
-            if (!QuoteLists)
-            {
-                this.quoted = false;
-            }
+			if (!QuoteLists)
+			{
+				this.quoted = false;
+			}
 
 			this.Head = head;
 			this.Rest = rest;
@@ -79,10 +105,10 @@ namespace Schemin.AST
 		{
 			if (this.Head == null)
 			{
-                ScheminList ret = new ScheminList();
-                ret.quoted = quoted;
+				ScheminList ret = new ScheminList();
+				ret.quoted = quoted;
 
-                return ret;
+				return ret;
 			}
 
 			return this.Head;
@@ -90,29 +116,29 @@ namespace Schemin.AST
 
 		public ScheminList Cdr()
 		{
-            ScheminList ret;
+			ScheminList ret;
 			if (this.Rest == null)
 			{
-                ret = new ScheminList();
-                ret.quoted = quoted;
+				ret = new ScheminList();
+				ret.quoted = quoted;
 
-                return ret;
+				return ret;
 			}
 
-            ret = this.Rest;
-            ret.quoted = quoted;
+			ret = this.Rest;
+			ret.quoted = quoted;
 
-            return ret;
+			return ret;
 		}
 
 		public ScheminList Cons(IScheminType type)
 		{
-            IScheminType oldHead = this.Head;
+			IScheminType oldHead = this.Head;
 
-            this.Head = type;
-            this.Rest = new ScheminList(oldHead, this.Rest);
+			this.Head = type;
+			this.Rest = new ScheminList(oldHead, this.Rest);
 
-            return this;
+			return this;
 		}
 
 		public ScheminList Append(IScheminType type)
@@ -197,20 +223,20 @@ namespace Schemin.AST
 			return builder.ToString();
 		}
 
-        public bool Quoted()
-        {
-            return this.quoted;
-        }
+		public bool Quoted()
+		{
+			return this.quoted;
+		}
 
-        public void Quote()
-        {
-            quoted = true;
-        }
+		public void Quote()
+		{
+			quoted = true;
+		}
 
-        public void UnQuote()
-        {
-            quoted = false;
-        }
+		public void UnQuote()
+		{
+			quoted = false;
+		}
 
 		public bool Equals(IScheminType type)
 		{
--- src/Schemin/Schemin.Parse/Parser.cs
+++ src/Schemin/Schemin.Parse/Parser.cs
@@ -1,3 +1,29 @@
+/* 
+ * Copyright (c) 2011 Alex Fort 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 namespace Schemin.Parse
 {
@@ -12,10 +38,10 @@ namespace Schemin.Parse
 	{
 		public ScheminList Parse(List<Token> tokens)
 		{
-            ScheminList.QuoteLists = false;
+			ScheminList.QuoteLists = false;
 			KeyValuePair<ScheminList, int> parsed = ParseInternal(tokens, 0);
 			TransformQuotes((ScheminList) parsed.Key);
-            ScheminList.QuoteLists = true;
+			ScheminList.QuoteLists = true;
 			return (ScheminList) parsed.Key;
 		}
 
@@ -51,7 +77,7 @@ namespace Schemin.Parse
 
 			return new KeyValuePair<ScheminList, int>(parsed, startIndex);
 		}
-				
+
 		private IScheminType ConvertToken(Token token)
 		{
 			switch (token.Type)
@@ -82,8 +108,8 @@ namespace Schemin.Parse
 					return new ScheminPrimitive(GeneralOperations.Define, "define");
 				case "quote":
 					return new ScheminPrimitive(GeneralOperations.Quote, "quote");
-                case "begin":
-                    return new ScheminPrimitive(GeneralOperations.Begin, "begin");
+				case "begin":
+					return new ScheminPrimitive(GeneralOperations.Begin, "begin");
 				case "if":
 					return new ScheminPrimitive(GeneralOperations.If, "if");
 				case "or":
@@ -100,8 +126,8 @@ namespace Schemin.Parse
 					return new ScheminPrimitive(GeneralOperations.LetStar, "let*");
 				case "set!":
 					return new ScheminPrimitive(GeneralOperations.SetBang, "set!");
-                case "call/cc":
-                    return new ScheminPrimitive(GeneralOperations.CallCC, "call/cc");
+				case "call/cc":
+					return new ScheminPrimitive(GeneralOperations.CallCC, "call/cc");
 				default:
 					return atom;
 			}
@@ -136,6 +162,6 @@ namespace Schemin.Parse
 				c = c.Rest;
 			}
 		}
-			
+
 	}
 }
